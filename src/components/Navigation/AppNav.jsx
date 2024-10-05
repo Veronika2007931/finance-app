f907b56389b7180cd2c23d0acff689c0933833ee
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2024-10-05 15:32:43 +0300

    add

---
+++ src/components/Navigation/AppNav.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom"
+export const AppNav= ()=>{
+  return(
+    <div>
+       <NavLink to="/registration"> Registration </NavLink>
+      <NavLink to="/login"> Login </NavLink>
+      <li></li>
+      {/* видали лі потім */}
+    </div>
+  )
+}
