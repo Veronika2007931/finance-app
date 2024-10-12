4f90e44723b4688084e3c9baba03c7bd9c347080
elka <112241103+Loskutov2@users.noreply.github.com>
2024-10-12 12:10:52 +0300

    commmitt

--- src/components/FinanceHistory/FinanceHistory.styled.jsx
+++ src/components/FinanceHistory/FinanceHistory.styled.jsx
@@ -1,4 +1,11 @@
 import styled from "styled-components"
 
 export const FinanceTable = styled.table`
+    button{
+        width: 32px;
+        height: 32px;
+        color: black;
+        border: none;
+        background-size: 18px;
+    }
 `
--- src/components/FinanceToggle/FinanceToggle.jsx
+++ src/components/FinanceToggle/FinanceToggle.jsx
@@ -1,11 +1,10 @@
-import { NavLink } from "react-router-dom"
 import { ToggleList } from "./FinanceToggle.styled"
 
 export const FinanceToggle = () => {
     return(
         <ToggleList>
-            <li><NavLink to='vytraty'>Витрати</NavLink></li>
-            <li><NavLink to='dohid'>Дохід</NavLink></li>
+            <li><a href="/">Витрати</a></li>
+            <li><a href="/">Дохід</a></li>
         </ToggleList>
     )
 }
