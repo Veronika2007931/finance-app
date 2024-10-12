a1f448997e643102f6a2ae3bfc3ddc822825a83e
elka <112241103+Loskutov2@users.noreply.github.com>
2024-10-12 12:51:59 +0300

    week2

--- src/components/FinanceHistory/FinanceHistory.styled.jsx
+++ src/components/FinanceHistory/FinanceHistory.styled.jsx
@@ -2,10 +2,19 @@ import styled from "styled-components"
 
 export const FinanceTable = styled.table`
     button{
-        width: 32px;
+        padding: 7px;
         height: 32px;
         color: black;
         border: none;
         background-size: 18px;
+        border-radius: 50%;
+        &:hover, &:focus{
+            color: #E7192E;
+            transform: scale(1.1);
+        }
+        svg{
+            width: 18px;
+            height: 18px;
+        }
     }
 `
--- src/components/FinanceToggle/FinanceToggle.styled.jsx
+++ src/components/FinanceToggle/FinanceToggle.styled.jsx
@@ -1,7 +1,20 @@
 import styled from "styled-components"
 
 export const ToggleList = styled.ul`
-    a.active{
-        color:#FF751D;
+    a{
+        width: 138px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #FAFBFD;
+        height: 40px;
+        border-radius: 16px 16px 0 0;
+        text-decoration: none;
+        font-weight: 700;
+        color: black;
     }
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
 `
