e2e81c1ed8030287ae5a9a2f06679af9170a3d44
Daniil <negativdaniil4@gmail.com>
2024-10-19 11:50:37 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,5 @@
 export const selectAuth = (state) => state.auth.isAuthenticated;
 export const selectUser = (state) => state.auth.user;
-export const selectBalance = (state) => state.balance;
-export const selectTransactions = (state) => state.transactions.transactions;
 export const selectFilters = (state) => state.filters;
 export const selectSpendings = (state) => state.user.spendings;
 export const selectIncomes = (state) => state.user.incomes;
