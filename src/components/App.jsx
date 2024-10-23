bdbb6a47ce5ae460f1db859d6feae573b54b071e
Настя Скрицька <snastia.m@gmail.com>
2024-10-23 17:32:52 +0300

    pnotify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,20 @@ import { error, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
-import { FinanceBlock } from "./FinanceBlock/FinanceBlock";
 
 import { Login } from './Header/Login/Login';
 import { Header } from './Header/Header'
 import { Modal } from "./Header/Login/Login.styled";
 import { Form, GoogleText, GoogleBtn, DefText, Input, LogInBtn, RegisterBtn } from "./Header/Login/Login.styled";
 defaultModules.set(PNotifyMobile, {});
-// import { Route, Routes } from "react-router-dom";
-// import { Layout } from "./Navigation/Layout";
-// import {HomePage} from "../Pages/HomePage"
-// import {RegPage} from "../Pages/RegPage"
-// import {LoginPage} from "../Pages/LoginPage"
-// import {Incomes} from "../Pages/Incomes"
-// import {Spendings} from "../Pages/Spendings"
-// import {FinanceFlow} from "../Pages/FinanceFlow"
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Navigation/Layout";
+import {HomePage} from "../Pages/HomePage"
+import {RegPage} from "../Pages/RegPage"
+import {LoginPage} from "../Pages/LoginPage"
+import {Incomes} from "../Pages/Incomes"
+import {Spendings} from "../Pages/Spendings"
+import {FinanceFlow} from "../Pages/FinanceFlow"
 
 
 export const App = () => {
@@ -49,17 +48,15 @@ const handleInpBtn = (e) => {
             <RegisterBtn>Реєстрація</RegisterBtn>
           </Form>
       </Modal>
-      <FinanceBlock/> 
-      {/* <FinanceBlock/>  */}
-      {/* <Layout/>
+      <Layout/>
       <Routes>
         <Route path="/"  element={<HomePage/>}/>
         <Route path="/registration" index element={<RegPage/>}/>
         <Route path="/loginization" element={<LoginPage/>}/>
-        {/* <Route path="/spendings" element={<SpendingsPage/>}/>
-        <Route path="/incomes" element={<IncomesPage/>}/> */}
-        {/* <Route path="/finance-flows" element={<FinanceFlow/>}/>
-      </Routes> */} 
+        <Route path="/spendings" element={<SpendingsPage/>}/>
+        <Route path="/incomes" element={<IncomesPage/>}/> 
+        <Route path="/finance-flows" element={<FinanceFlow/>}/>
+      </Routes> 
       
       
       
