f387b42b387e914210ea5d24fc607ddf5cf9ec3b
elka <112241103+Loskutov2@users.noreply.github.com>
2024-11-16 12:13:45 +0200

    final100%

--- src/components/FinanceHistory/FinanceHistory.jsx
+++ src/components/FinanceHistory/FinanceHistory.jsx
@@ -22,7 +22,7 @@ export const FinanceHistory = ({isSpendings, finanseList, delfunc}) => {
                 {Array.isArray(finanseList)&&finanseList.map(item=>
                     <tr key={item.id}>
                         <td>{item.date}</td>
-                        <td>{item.desciption}{console.log(item.desciption)}</td>
+                        <td>{item.description}</td>
                         <td>{item.category}</td>
                         <td style={{fontWeight: 700,color: isSpendings?'#E7192E':'rgba(64, 121, 70, 1)'}}>{isSpendings&&"-"}{item.sum}</td>
                         <DelCol><button type="button" onClick={()=>{onDelete(item.id)}}><BsTrash3/></button></DelCol>
