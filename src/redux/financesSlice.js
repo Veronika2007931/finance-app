5622e93cf4067379bad14882e93aaf34d4d5eb6d
Daniil <negativdaniil4@gmail.com>
2024-12-04 23:32:36 +0200

    Update financesSlice.js

--- src/redux/financesSlice.js
+++ src/redux/financesSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   spendings: [
     {id: '1', date:"2010-02-02", description:"paracetamol", category:"meds", sum:2233},
-    {id: '2', date:"2021-08-23", description:"cheeze", category:"food", sum:45}
+    {id: '2', date:"2021-08-23", description:"cheeze", category:"food", sum:45},
+    {id: '3', date:"2021-08-23", description:"gym", category:"entertainment", sum:700},
   ],
   incomes: [
-    {id: '3' ,date:"2024-01-05", description:"kolyadka", category:"beggary", sum:500},
-    {id: '4', date:"2047-06-31", description:"sallary", category:"job", sum:45000}
+    {id: '4' ,date:"2024-01-05", description:"kolyadka", category:"beggary", sum:500},
+    {id: '5', date:"2047-06-31", description:"sallary", category:"job", sum:45000},
+    {id: '6', date:"2022-12-23", description:"birthday", category:"entertainment", sum:5000},
   ]
 }
 
